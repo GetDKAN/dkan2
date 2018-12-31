6d12d6da9f314389bd730eadd18971666d2100fb
fmizzell <fmizzell@1312210.no-reply.drupal.org>
2018-12-31 15:37:56 -0600

    Adding index functionality to the dataset API.

--- modules/custom/dkan_api/src/Storage/DrupalNode.php
+++ modules/custom/dkan_api/src/Storage/DrupalNode.php
@@ -3,8 +3,10 @@
 namespace Drupal\dkan_api\Storage;
 
 use Drupal\node\Entity\Node;
+use Sae\Contracts\Storage;
+use Sae\Contracts\BulkRetriever;
 
-abstract class DrupalNode implements \Sae\Contracts\Storage
+abstract class DrupalNode implements Storage, BulkRetriever
 {
   abstract protected function getType();
 
@@ -21,6 +23,21 @@ abstract class DrupalNode implements \Sae\Contracts\Storage
 
     throw new \Exception("No data with the identifier {$id} was found.");
   }
+
+  public function retrieveAll() {
+    $connection = \Drupal::database();
+    $sql = "SELECT nid FROM node WHERE type = :type";
+    $query = $connection->query($sql, [':type' => $this->getType()]);
+    $results = $query->fetchAll();
+
+    $all = [];
+    foreach ($results as $result) {
+      $node = Node::load($result->nid);
+      $all[] = $node->field_json_metadata->value;
+    }
+    return "[" . implode(",", $all) . "]";
+  }
+
 
   public function store($data, $id = Null) {
     $node = Node::create(['type' => $this->getType(), 'field_json_metadata' => $data]);
