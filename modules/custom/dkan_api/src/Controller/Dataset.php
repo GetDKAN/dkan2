869cca5bc4ac8a4bed09f966d3e1a1347b62155d
fmizzell <fmizzell@users.noreply.github.com>
2019-02-19 09:25:37 -0600

    Efforts toward a fully validated path in and out of DKAN (#62)

--- modules/custom/dkan_api/src/Controller/Dataset.php
+++ modules/custom/dkan_api/src/Controller/Dataset.php
@@ -3,6 +3,8 @@
 namespace Drupal\dkan_api\Controller;
 
 use Drupal\dkan_api\Storage\DrupalNodeDataset;
+use Drupal\dkan_schema\SchemaRetriever;
+use JsonSchemaProvider\Provider;
 
 class Dataset extends Api {
 
@@ -22,70 +24,7 @@ class Dataset extends Api {
   }
 
   protected function getJsonSchema() {
-    return "
-    {
-      \"title\": \"Dataset\",
-      \"description\": \"A simple dataset.\",
-      \"type\": \"object\",
-      \"required\": [
-        \"title\"
-      ],
-      \"properties\": {
-        \"title\": {
-          \"type\": \"string\",
-          \"title\": \"Title\"
-        },
-        \"identifier\": {
-          \"type\": \"string\",
-          \"title\": \"Identifier\",
-          \"description\": \"Unique identifier for dataset.\"
-        },
-        \"description\": {
-          \"type\": \"string\",
-          \"title\": \"Description\"
-        },
-        \"organization\": {
-          \"type\": \"string\",
-          \"title\": \"Organization\"
-        },
-        \"created\": {
-          \"type\": \"string\",
-          \"title\": \"Created\",
-          \"format\": \"date-time\"
-        },
-        \"modified\": {
-          \"type\": \"string\",
-          \"title\": \"Modified\",
-          \"format\": \"date-time\"
-        },
-        \"resources\": {
-          \"type\": \"array\",
-          \"title\": \"Resources\",
-          \"items\": {
-            \"type\": \"object\",
-            \"properties\": {
-              \"title\": {
-                \"type\": \"string\",
-                \"title\": \"Title\"
-              },
-              \"uri\": {
-                \"type\": \"string\",
-                \"title\": \"URI\"
-              },
-              \"type\": {
-                \"type\": \"string\",
-                \"title\": \"Type\",
-                \"enum\": [
-                  \"csv\",
-                  \"html\",
-                  \"xls\"
-                ]
-              }
-            }
-          }
-        }
-      }
-    }
-    ";
+    $provider = new Provider(new SchemaRetriever());
+    return $provider->retrieve('dataset');
   }
 }
