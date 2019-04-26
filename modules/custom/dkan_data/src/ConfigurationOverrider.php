52a6b2ca5879977da2df87aea9a74bb9d3738499
fmizzell <fmizzell.dev@gmail.com>
2019-04-26 16:26:30 -0500

    Dynamically loading json schema form configuration, and connecting validation and other metadata management to Drupal's entity management pipes.

---
+++ modules/custom/dkan_data/src/ConfigurationOverrider.php
@@ -0,0 +1,45 @@
+<?php
+namespace Drupal\dkan_data;
+
+use Drupal\Core\Config\ConfigFactoryOverrideInterface;
+use Drupal\Core\Config\StorageInterface;
+use Drupal\dkan_schema\SchemaRetriever;
+
+class ConfigurationOverrider implements ConfigFactoryOverrideInterface {
+
+  public function loadOverrides($names) {
+    if (in_array("core.entity_form_display.node.data.default", $names)) {
+      $retriever = new SchemaRetriever();
+      $schema = $retriever->retrieve("dataset");
+      return [
+        "core.entity_form_display.node.data.default" =>
+        [
+          'content' =>
+          [
+            'field_json_metadata' =>
+            [
+              'settings' =>
+              [
+                'json_form' => $schema
+              ]
+            ]
+          ]
+        ]
+      ];
+    }
+    return [];
+  }
+
+  public function getCacheSuffix() {
+    $blah = "blah";
+  }
+
+  public function createConfigObject($name, $collection = StorageInterface::DEFAULT_COLLECTION) {
+    $blah = "blah";
+  }
+
+  public function getCacheableMetadata($name) {
+    $blah = "blah";
+  }
+
+}
---
+++ modules/custom/dkan_data/src/Plugin/Validation/Constraint/ProperJsonValidator.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace Drupal\dkan_data\Plugin\Validation\Constraint;
+
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+class ProperJsonValidator extends ConstraintValidator {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validate($items, Constraint $constraint) {
+    foreach ($items as $item) {
+      $info = $this->isProper($item->value);
+      if (!$info['valid']) {
+        $this->addViolations($info['errors']);
+      }
+    }
+  }
+
+  /**
+   * Is proper JSON?
+   *
+   * @param string $value
+   */
+  private function isProper($value) {
+    /** @var $controller \Drupal\dkan_api\Controller\Dataset **/
+    $controller = \Drupal::service("dkan_api.controller.dataset");
+    $engine = $controller->getEngine();
+    return $engine->validate($value);
+  }
+
+  /**
+   * Add Violations.
+   */
+  private function addViolations($errors) {
+    foreach ($errors as $error) {
+      $this->context->addViolation($error['message']);
+    }
+  }
+
+}
