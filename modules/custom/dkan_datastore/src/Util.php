41751bae2e15caae5a840a6a3ad9834172e61879
Yaasir Ketwaroo <yaasir.ketwaroo@semanticbits.com>
2019-06-03 14:11:23 -0400

    https://github.com/GetDKAN/dkan2/pull/126#discussion_r289909858

--- modules/custom/dkan_datastore/src/Util.php
+++ modules/custom/dkan_datastore/src/Util.php
@@ -23,22 +23,6 @@ class Util {
     /** @var Manager\DatastoreManagerBuilder $builder */
     $builder = \Drupal::service('dkan_datastore.manager.datastore_manager_builder');
     return $builder->build($uuid);
-
-    // $database = \Drupal::service('dkan_datastore.database');
-    //
-    //    $dataset = \Drupal::service('entity.repository')
-    //      ->loadEntityByUuid('node', $uuid);
-    //
-    //    $metadata = json_decode($dataset->field_json_metadata->value);
-    //    $resource = new Resource($dataset->id(), $metadata->distribution[0]->downloadURL);
-    //
-    //    $provider = new InfoProvider();
-    //    $provider->addInfo(new Info(SimpleImport::class, "simple_import", "SimpleImport"));
-    //
-    //    $bin_storage = new LockableBinStorage("dkan_datastore", new Locker("dkan_datastore"), \Drupal::service('dkan_datastore.storage.variable'));
-    //    $factory = new Factory($resource, $provider, $bin_storage, $database);
-    //
-    //    return $factory->get();
   }
 
 }
