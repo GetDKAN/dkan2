d7cb0b2d3a2b8631ae2444ebbb4b8cd4829cd32d
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-06-24 11:57:52 -0700

    Handle datasets with multiple resources or just a single resource object

--- modules/custom/dkan_datastore/src/Commands/DkanDatastoreCommands.php
+++ modules/custom/dkan_datastore/src/Commands/DkanDatastoreCommands.php
@@ -11,6 +11,7 @@ use Drush\Commands\DrushCommands;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Dkan\Datastore\Manager\SimpleImport\SimpleImport;
 use Dkan\Datastore\Resource;
+use Drupal\dkan_data\ValueReferencer;
 
 /**
  * @codeCoverageIgnore
@@ -39,6 +40,8 @@ class DkanDatastoreCommands extends DrushCommands {
    */
   public function import($uuid, $deferred = FALSE) {
     try {
+      // Load metadata with both identifier and data for this request.
+      drupal_static('dkan_data_dereference_method', ValueReferencer::DEREFERENCE_OUTPUT_BOTH);
       $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
       $nodes = $nodeStorage->loadByProperties([
         'uuid' => $uuid,
@@ -60,10 +63,26 @@ class DkanDatastoreCommands extends DrushCommands {
         return;
       }
 
-        $dataset = $node;
+      // Standardize whether single resource object or several in a dataset.
+      $metadata = json_decode($node->field_json_metadata->value);
+      $distributions = [];
+      if ($node->field_data_type->value == 'dataset') {
+        $distributions = $metadata->distribution;
+      }
+      if ($node->field_data_type->value == 'distribution') {
+        $distributions[] = $metadata;
+      }
 
-        $metadata = json_decode($dataset->field_json_metadata->value);
-        $resource = new Resource($dataset->id(), $metadata->distribution[0]->downloadURL);
+      foreach ($distributions as $dist) {
+        // Use this distribution's nid to decorate the dkan_datastore sql table.
+        $dist_nodes = $nodeStorage->loadByProperties(['uuid' => $dist->identifier]);
+        $dist_node = reset($dist_nodes);
+        if (!$dist_node) {
+          $this->output->writeln("Unable to find thus skipping distribution node {$dist->identifier}.");
+          continue;
+        }
+        $resource = new Resource($dist_node->id(), $dist->data->downloadURL);
+
         // Handle the command differently if deferred.
         if (!empty($deferred)) {
           /** @var \Drupal\dkan_datastore\Manager\DeferredImportQueuer $deferredImporter */
@@ -82,6 +101,7 @@ class DkanDatastoreCommands extends DrushCommands {
           $datastore = $factory->get();
           $datastore->import();
         }
+      }
     }
     catch (\Exception $e) {
       $this->output->writeln("We were not able to load the entity with uuid {$uuid}");
@@ -99,6 +119,8 @@ class DkanDatastoreCommands extends DrushCommands {
    */
   public function drop($uuid) {
     try {
+      // Load metadata with both identifier and data for this request.
+      drupal_static('dkan_data_dereference_method', ValueReferencer::DEREFERENCE_OUTPUT_BOTH);
       $nodeStorage = \Drupal::entityTypeManager()->getStorage('node');
       $nodes = $nodeStorage->loadByProperties([
         'uuid' => $uuid,
@@ -120,19 +142,36 @@ class DkanDatastoreCommands extends DrushCommands {
         return;
       }
 
-        $database = \Drupal::service('dkan_datastore.database');
-        $dataset = $node;
+      // Standardize whether single resource object or several in a dataset.
+      $metadata = json_decode($node->field_json_metadata->value);
+      $distributions = [];
+      if ($node->field_data_type->value == 'dataset') {
+        $distributions = $metadata->distribution;
+      }
+      if ($node->field_data_type->value == 'distribution') {
+        $distributions[] = $metadata;
+      }
 
-        $metadata = json_decode($dataset->field_json_metadata->value);
-        $resource = new Resource($dataset->id(), $metadata->distribution[0]->downloadURL);
+      foreach ($distributions as $dist) {
+        // Use this distribution's nid to decorate the dkan_datastore sql table.
+        $dist_nodes = $nodeStorage->loadByProperties(['uuid' => $dist->identifier]);
+        $dist_node = reset($dist_nodes);
+        if (!$dist_node) {
+          $this->output->writeln("Unable to find thus skipping distribution node {$dist->identifier}.");
+          continue;
+        }
+        $resource = new Resource($dist_node->id(), $dist->data->downloadURL);
+
         $provider = new InfoProvider();
         $provider->addInfo(new Info(SimpleImport::class, "simple_import", "SimpleImport"));
         $bin_storage = new LockableBinStorage("dkan_datastore", new Locker("dkan_datastore"), \Drupal::service('dkan_datastore.storage.variable'));
+        $database = \Drupal::service('dkan_datastore.database');
         $factory = new Factory($resource, $provider, $bin_storage, $database);
 
         /* @var $datastore \Dkan\Datastore\Manager\SimpleImport\SimpleImport */
         $datastore = $factory->get();
         $datastore->drop();
+      }
     }
     catch (\Exception $e) {
       $this->output->writeln("We were not able to load the entity with uuid {$uuid}");
