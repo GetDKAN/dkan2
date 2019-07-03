4b6d51651c16b43cd5b39a249b3590b6008b7457
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-07-03 15:15:27 -0700

    Fix typo

--- cypress/integration/datastore.spec.js
+++ cypress/integration/datastore.spec.js
@@ -1,7 +1,7 @@
 context('Datastore Api', () => {
 
-    let endpoint = "http://dkan/api/v1/datastore"
-    let dataset_identifier = "c9e2d352-e24c-4051-9158-f48127aa5692"
+    let endpoint = "http://dkan/api/v1/datastore";
+    let dataset_identifier = "c9e2d352-e24c-4051-9158-f48127aa5692";
 
     context('Headers and statistics', () => {
         it('headers', () => {
@@ -15,20 +15,20 @@ context('Datastore Api', () => {
                 "dari_prov",
                 "dist_id",
                 "prov_id"
-            ]
+            ];
 
             cy.request(endpoint + '/' + dataset_identifier + '?values=both').then((response) => {
-                expect(response.status).eql(200)
+                expect(response.status).eql(200);
                 expect(response.body.columns).eql(expected_columns)
             })
-        })
+        });
         it('statistics', () => {
             cy.request(endpoint + '/' + dataset_identifier + '?values=both').then((response) => {
-                expect(response.status).eql(200)
-                expect(response.body.datastore_statistics.rows).eql('399')
+                expect(response.status).eql(200);
+                expect(response.body.datastore_statistics.rows).eql('399');
                 expect(response.body.datastore_statistics.columns).eql(9)
             })
         })
     })
 
-})
+});
