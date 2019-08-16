5c1733d19218351c32a598763e2984497aa83eda
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-08-16 06:21:42 -0700

    Add dredd hooks to inject basic auth's 64-bit encoded username:password

---
+++ dredd-hooks.js
@@ -0,0 +1,42 @@
+/*
+ * Hooks to add or modify request information.
+ */
+
+const hooks = require('hooks');
+
+/*
+ * Using Dredd hooks to set the 64-bit encoded username:password for basic
+ * authorization.
+ *
+ * Another attempt at doing this via dredd.yml's `user` option did not work as
+ * intended since it applied it indiscriminately for all request paths and
+ * verbs.
+ */
+
+// Hooks for authenticated requests for dataset and property endpoints
+hooks.before('/api/v1/dataset > Create a dataset > 201 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+hooks.before('/api/v1/dataset/{uuid} > Replace a dataset > 200 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+hooks.before('/api/v1/dataset/{uuid} > Update a dataset > 200 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+hooks.before('/api/v1/dataset/{uuid} > Delete a dataset > 200 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+
+// Hooks for property endpoints
+hooks.before('/api/v1/{property} > Create a property > 201 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+hooks.before('/api/v1/{property} > Replace a property > 200 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+hooks.before('/api/v1/{property} > Update a property > 200 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
+hooks.before('/api/v1/{property} > Delete a property > 200 > application/json', (transaction) => {
+  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
+});
