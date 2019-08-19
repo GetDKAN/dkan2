09d139e3a1020bb266264d991b2f7d3de732ef9e
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-08-19 12:49:34 -0700

    Refactor endpoints requiring auth in dredd hooks

--- dredd-hooks.js
+++ dredd-hooks.js
@@ -37,31 +37,21 @@ endpointsToSkip.forEach(endpoint => hooks.before(endpoint, (transaction) => {
  * intended since it applied it indiscriminately for all request paths and
  * verbs.
  */
-
-// Hooks for authenticated requests for dataset and property endpoints
-hooks.before('/api/v1/dataset > Create a dataset > 201 > application/json', (transaction) => {
+endpointsRequiringAuth = [
+  '/api/v1/dataset > Create a dataset > 201 > application/json',
+  '/api/v1/dataset/{uuid} > Replace a dataset > 200 > application/json',
+  '/api/v1/dataset/{uuid} > Update a dataset > 200 > application/json',
+  '/api/v1/dataset/{uuid} > Delete a dataset > 200 > application/json',
+  '/api/v1/{property} > Create a property > 201 > application/json',
+  '/api/v1/{property}/{uuid} > Replace a property > 200 > application/json',
+  '/api/v1/{property}/{uuid} > Update a property > 200 > application/json',
+  '/api/v1/{property}/{uuid} > Delete a property > 200 > application/json',
+  '/api/v1/harvest > Register a new harvest > 200 > application/json',
+  '/api/v1/harvest/run/{id} > Runs a harvest > 200 > application/json',
+  '/api/v1/datastore/{uuid} > Drop a datastore > 200 > application/json',
+  '/api/v1/datastore/import/{uuid} > Datastore import > 200 > application/json',
+  '/api/v1/datastore/import/{uuid}/deferred > Deferred datastore import > 200 > application/json',
+];
+endpointsRequiringAuth.forEach(endpoint => hooks.before(endpoint, (transaction) => {
   transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-hooks.before('/api/v1/dataset/{uuid} > Replace a dataset > 200 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-hooks.before('/api/v1/dataset/{uuid} > Update a dataset > 200 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-hooks.before('/api/v1/dataset/{uuid} > Delete a dataset > 200 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-
-// Hooks for property endpoints
-hooks.before('/api/v1/{property} > Create a property > 201 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-hooks.before('/api/v1/{property}/{uuid} > Replace a property > 200 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-hooks.before('/api/v1/{property}/{uuid} > Update a property > 200 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
-hooks.before('/api/v1/{property}/{uuid} > Delete a property > 200 > application/json', (transaction) => {
-  transaction.request.headers.Authorization = 'Basic dGVzdHVzZXI6Mmpxek9BblhTOW1tY0xhc3k=';
-});
+}));
