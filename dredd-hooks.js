ed6f055ad7eed6847bf4996efbeaeb9455da2e87
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-08-19 12:25:29 -0700

    Skip tests requiring new or referenced data

--- dredd-hooks.js
+++ dredd-hooks.js
@@ -5,6 +5,31 @@
 const hooks = require('hooks');
 
 /*
+ * Skip the following tests until our database contains more relevant data,
+ * or we can create some as needed.
+ */
+endpointsToSkip = [
+  '/api/v1/{property} > Create a property > 201 > application/json',
+  '/api/v1/{property}/{uuid} > Get a property > 200 > application/json',
+  '/api/v1/{property}/{uuid} > Replace a property > 200 > application/json',
+  '/api/v1/{property}/{uuid} > Update a property > 200 > application/json',
+  '/api/v1/{property}/{uuid} > Delete a property > 200 > application/json',
+  '/api/v1/sql/{query} > Query resources in datastore > 200 > application/json',
+  '/api/v1/harvest > Register a new harvest > 200 > application/json',
+  '/api/v1/harvest/info/{id} > List previous runs for a harvest id > 200 > application/json',
+  '/api/v1/harvest/info/{id}/{run_id} > Information about a specific previous harvest run > 200 > application/json',
+  '/api/v1/harvest/info/{id}/{run_id} > Information about a specific previous harvest run > 200 > application/json',
+  '/api/v1/harvest/run/{id} > Runs a harvest > 200 > application/json',
+  '/api/v1/datastore/{uuid} > Return a dataset with datastore headers and statistics > 200 > application/json',
+  '/api/v1/datastore/{uuid} > Drop a datastore > 200 > application/json',
+  '/api/v1/datastore/import/{uuid} > Datastore import > 200 > application/json',
+  '/api/v1/datastore/import/{uuid}/deferred > Deferred datastore import > 200 > application/json',
+];
+endpointsToSkip.forEach(endpoint => hooks.before(endpoint, (transaction) => {
+  transaction.skip = true;
+}));
+
+/*
  * Using Dredd hooks to set the 64-bit encoded username:password for basic
  * authorization.
  *
