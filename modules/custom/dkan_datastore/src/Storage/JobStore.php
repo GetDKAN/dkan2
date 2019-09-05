b00dfebcfa8570874dc7b22d93c543617bf85e7e
Dan Feder <dafeder@gmail.com>
2019-09-05 17:02:33 -0400

    Add jobStore as property to Datastore service, add list capabilities.

--- modules/custom/dkan_datastore/src/Storage/JobStore.php
+++ modules/custom/dkan_datastore/src/Storage/JobStore.php
@@ -12,6 +12,11 @@ use Drupal\Core\Database\Connection;
  */
 class JobStore {
 
+  /**
+   * The database connection to use for querrying jobstore tables.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
   private $connection;
 
   /**
@@ -24,14 +29,14 @@ class JobStore {
   /**
    * Get.
    */
-  public function get(string $uuid, string $jobClass) {
+  public function retrieve(string $uuid, string $jobClass) {
     if (!$this->validateJobClass($jobClass)) {
       throw new \Exception("Invalid jobType provided: $jobClass");
     }
 
     $tableName = $this->getTableName($jobClass);
     if (!$this->tableExists($tableName)) {
-      $this->createTable($tableName);
+      throw new \Exception("Table not found: $tableName");
     }
 
     $result = $this->connection->select($tableName, 't')
@@ -45,6 +50,36 @@ class JobStore {
     if (isset($job) && ($job instanceof $jobClass)) {
       return $job;
     }
+  }
+
+  /**
+   * Retrieve all.
+   */
+  public function retrieveAll(string $jobClass): array {
+    if (!$this->validateJobClass($jobClass)) {
+      throw new \Exception("Invalid jobType provided: $jobClass");
+    }
+
+    $tableName = $this->getTableName($jobClass);
+    if (!$this->tableExists($tableName)) {
+      throw new \Exception("Table not found: $tableName");
+    }
+
+    $result = $this->connection->select($tableName, 't')
+      ->fields('t', ['ref_uuid', 'job_data'])
+      ->execute()
+      ->fetchAll();
+    if (empty($result)) {
+      throw new \Exception("No data in table: $tableName");
+    }
+    $jobs = [];
+    foreach ($result as $row) {
+      $job = $jobClass::hydrate($row->job_data);
+      if (isset($job) && ($job instanceof $jobClass)) {
+        $jobs[$row->ref_uuid] = $job;
+      }
+    }
+    return $jobs;
   }
 
   /**
