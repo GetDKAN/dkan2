7979b7e11ecf5f6b2ce5bdd73b23ade0e5d64b4d
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-09-28 10:41:17 -0700

    Update Cypress datastore tests

--- cypress/integration/datastore.spec.js
+++ cypress/integration/datastore.spec.js
@@ -1,83 +1,76 @@
 context('Datastore API', () => {
-  let expected_columns = [
-    "lon",
-    "lat",
-    "unit_type",
-    "dist_name",
-    "prov_name",
-    "dari_dist",
-    "dari_prov",
-    "dist_id",
-    "prov_id"
-  ];
-  let datastore_endpoint = "http://dkan/api/v1/datastore";
-  let dataset_endpoint = "http://dkan/api/v1/dataset";
-  let dataset_identifier = "c9e2d352-e24c-4051-9158-f48127aa5692";
-  let resource_identifier = null
-  let user_credentials = {
-    user: 'testuser',
-    pass: '2jqzOAnXS9mmcLasy'
-  };
+  let expected_columns;
+  let resource_identifier;
+  let user_credentials = Cypress.env('TEST_USER_CREDENTIALS');
 
   before(() => {
-    cy.request(dataset_endpoint + '/' + dataset_identifier + '?view=verbose').then((response) => {
-      resource_identifier = response.body.distribution[0].identifier
+    cy.fixture('electionDistricts').then((json) => {
+      cy.request('metastore/schemas/dataset/items/' + json.uuid + '?show-reference-ids').then((response) => {
+        expect(response.status).eql(200);
+        resource_identifier = response.body.distribution[0].identifier;
+        expect(resource_identifier).to.match(new RegExp(Cypress.env('UUID_REGEX')));
+      });
+      expected_columns = json.properties
     })
-  })
+  });
 
-  it('GET - Empty', () => {
+  it('List imports', () => {
     cy.request({
-        url: datastore_endpoint + '/' + resource_identifier,
-        failOnStatusCode: false
-      }
-    ).then((response) => {
-      expect(response.body.message).eql("A datastore for resource " + resource_identifier + " does not exist.")
+      url: 'datastore/imports',
+      auth: user_credentials
+    }).then((response) => {
+      let firstKey = Object.keys(response.body)[0];
+      expect(response.status).eql(200);
+      expect(response.body[firstKey].hasOwnProperty('fileFetcher')).equals(true);
+      expect(response.body[firstKey].hasOwnProperty('fileFetcherStatus')).equals(true);
+      expect(response.body[firstKey].hasOwnProperty('fileName')).equals(true);
     })
-  })
+  });
 
   it('Import, Get Info, and Delete', () => {
     cy.request({
-        method: 'PUT',
-        url: datastore_endpoint +'/import/' + resource_identifier,
-        auth: user_credentials
+      method: 'POST',
+      url: 'datastore/imports',
+      auth: user_credentials,
+      body: {
+        "resource_id": resource_identifier
+      }
     }).then((response) => {
       expect(response.status).eql(200);
       expect(response.body.FileFetcherResult.status).eql("done");
       expect(response.body.ImporterResult.status).eql("done");
-    })
+    });
 
-    cy.request(datastore_endpoint + '/' + resource_identifier).then((response) => {
+    cy.request('datastore/imports/' + resource_identifier).then((response) => {
       expect(response.status).eql(200);
-      expect(response.body.columns).eql(expected_columns)
+      expect(response.body.columns).eql(expected_columns);
       expect(response.body.numOfRows).eql(399);
-      expect(response.body.numOfColumns).eql(9)
-    })
+      expect(response.body.numOfColumns).eql(9);
+    });
 
     cy.request({
-        method: 'DELETE',
-        url: datastore_endpoint +'/' + resource_identifier,
-        auth: user_credentials
+      method: 'DELETE',
+      url: 'datastore/imports/' + resource_identifier,
+      auth: user_credentials
     }).then((response) => {
       expect(response.status).eql(200);
-    })
+    });
+  });
 
+  it('GET empty', () => {
     cy.request({
-        url: datastore_endpoint + '/' + resource_identifier,
-        failOnStatusCode: false
+      url: 'datastore/imports/' + resource_identifier,
+      failOnStatusCode: false
     }).then((response) => {
       expect(response.body.message).eql("A datastore for resource " + resource_identifier + " does not exist.")
     })
+  });
 
-  })
-
-  it('PUT - Deferred Import', () => {
-    cy.request({
-      method: 'PUT',
-      url: datastore_endpoint +'/import/' + resource_identifier + '/deferred',
-      auth: user_credentials
-    }).then((response) => {
-        expect(response.status).eql(200);
+  it('GET openapi api spec', () => {
+    cy.request('datastore').then((response) => {
+      expect(response.status).eql(200);
+      expect(response.body.hasOwnProperty('openapi')).equals(true);
     })
-  })
+  });
 
 });
