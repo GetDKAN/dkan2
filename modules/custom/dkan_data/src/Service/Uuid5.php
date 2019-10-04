ce38341f1838a997fcf8eadfb59a8886a955c744
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-10-04 11:46:17 -0700

    Include schema id and data value to generate uuid5

--- modules/custom/dkan_data/src/Service/Uuid5.php
+++ modules/custom/dkan_data/src/Service/Uuid5.php
@@ -8,23 +8,30 @@ use Ramsey\Uuid\Uuid;
 
 /**
  * Service to generate predictable uuid's.
+ *
+ * We cannot solely rely on the data value. A keyword and a theme described
+ * by the same string must not end up being the same uuid, nor reference.
+ * Therefore, we create the named identifier from concatenating the schema id
+ * and the data value.
  */
 class Uuid5 {
 
   /**
    * Generate a uuid version 5.
    *
+   * @param $schema_id
+   *   The schema id of this value.
    * @param mixed $value
    *   The value for which we generate a uuid for.
    *
    * @return string
    *   The uuid.
    */
-  public function generate($value) {
+  public function generate($schema_id, $value) {
     if (!is_string($value)) {
       $value = json_encode($value, JSON_UNESCAPED_SLASHES);
     }
-    $uuid = Uuid::uuid5(Uuid::NAMESPACE_DNS, $value);
+    $uuid = Uuid::uuid5(Uuid::NAMESPACE_DNS, $schema_id . ":" . $value);
     return $uuid->toString();
   }
 
