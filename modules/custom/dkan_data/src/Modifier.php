b0d392c5112a75b64a81cae8e45c5b038abbd7dc
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2019-12-26 10:00:46 -0800

    Add a data modifier service

---
+++ modules/custom/dkan_data/src/Modifier.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\dkan_data;
+
+/**
+ * Class Modifier.
+ *
+ * @package Drupal\dkan_data
+ */
+class Modifier implements ModifierInterface {
+
+  /**
+   * No changes by default, can be modified with a decorator service.
+   *
+   * {@inheritDoc}
+   */
+  public function modifyGet(string $schema_id, $data) {
+    return $data;
+  }
+
+  /**
+   * No changes by default, can be modified with a decorator service.
+   *
+   * {@inheritDoc}
+   */
+  public function modifyResources($resources) {
+    return $resources;
+  }
+
+}
---
+++ modules/custom/dkan_data/src/ModifierInterface.php
@@ -0,0 +1,40 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\dkan_data;
+
+/**
+ * Interface ModifierInterface.
+ *
+ * Potentially modify the output of unauthenticated metastore API requests.
+ *
+ * @package Drupal\dkan_data
+ */
+interface ModifierInterface {
+
+  /**
+   * Modify the output of Metastore's Get.
+   *
+   * @param string $schema_id
+   *   The {schema_id} slug from the HTTP request.
+   * @param mixed $data
+   *   The data whose output is to potentially be modified.
+   *
+   * @return mixed
+   *   The modified data.
+   */
+  public function modifyGet(string $schema_id, $data);
+
+  /**
+   * Modify the output of Metastore's getResources.
+   *
+   * @param mixed $resources
+   *   A dataset's resources.
+   *
+   * @return mixed
+   *   The modified resources.
+   */
+  public function modifyResources($resources);
+
+}
