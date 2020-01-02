fce8cd205464e48522ab2749dcd5f3034ae18d1f
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-01-02 04:58:27 -0800

    Add an abstract base class for data protector plugins

---
+++ modules/custom/dkan_data/src/Plugin/DataProtectorBase.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Drupal\dkan_data\Plugin;
+
+use Drupal\Component\Plugin\PluginBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Base class for Data protector plugins.
+ */
+abstract class DataProtectorBase extends PluginBase implements DataProtectorInterface, ContainerFactoryPluginInterface {
+
+  /**
+   * Drupal\Core\Entity\EntityTypeManagerInterface definition.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    return $instance;
+  }
+
+  // @Todo: Add common and abstract methods for this plugin type.
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginId() {
+
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPluginDefinition() {
+
+  }
+
+}
