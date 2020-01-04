a42cd20cd3fce53a8c816fe6a45f23e74511fc02
Thierry Dallacroce <thierry.dallacroce@gmail.com>
2020-01-04 08:07:33 -0800

    Add a data protector plugin for dataset-specific docs api endpoint

---
+++ modules/custom/dkan_data/src/Annotation/DataProtectorApiDocs.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Drupal\dkan_data\Annotation;
+
+/**
+ * Annotation of data protectors plugins for DKAN api docs endpoints.
+ *
+ * @package Drupal\dkan_data\Annotation
+ *
+ * @Annotation
+ */
+class DataProtectorApiDocs extends DataProtectorBase {
+}
