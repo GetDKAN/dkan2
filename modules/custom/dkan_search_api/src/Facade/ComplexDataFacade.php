a256467039226842d7287eea47cd4bcce3a08b1b
fmizzell <fmizzell.dev@gmail.com>
2020-01-14 23:45:54 -0600

    First draft of search api integration.

---
+++ modules/custom/dkan_search_api/src/Facade/ComplexDataFacade.php
@@ -0,0 +1,206 @@
+<?php
+namespace Drupal\dkan_search_api\Facade;
+
+use Drupal\Core\TypedData\ComplexDataInterface;
+use Drupal\Core\TypedData\TraversableTypedDataInterface;
+
+class ComplexDataFacade implements \Iterator, ComplexDataInterface
+{
+
+  /**
+   * @inheritDoc
+   */
+  public function getDataDefinition()
+  {
+    // TODO: Implement getDataDefinition() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function get($property_name)
+  {
+    // TODO: Implement get() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function set($property_name, $value, $notify = true)
+  {
+    // TODO: Implement set() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getProperties($include_computed = false)
+  {
+    // TODO: Implement getProperties() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function toArray()
+  {
+    // TODO: Implement toArray() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function isEmpty()
+  {
+    // TODO: Implement isEmpty() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function current()
+  {
+    // TODO: Implement current() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function next()
+  {
+    // TODO: Implement next() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function key()
+  {
+    // TODO: Implement key() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function valid()
+  {
+    // TODO: Implement valid() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function rewind()
+  {
+    // TODO: Implement rewind() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function onChange($name)
+  {
+    // TODO: Implement onChange() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public static function createInstance(
+    $definition,
+    $name = null,
+    TraversableTypedDataInterface $parent = null
+  ) {
+    // TODO: Implement createInstance() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getValue()
+  {
+    // TODO: Implement getValue() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setValue($value, $notify = true)
+  {
+    // TODO: Implement setValue() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getString()
+  {
+    // TODO: Implement getString() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getConstraints()
+  {
+    // TODO: Implement getConstraints() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function validate()
+  {
+    // TODO: Implement validate() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function applyDefaultValue($notify = true)
+  {
+    // TODO: Implement applyDefaultValue() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getName()
+  {
+    // TODO: Implement getName() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getParent()
+  {
+    // TODO: Implement getParent() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getRoot()
+  {
+    // TODO: Implement getRoot() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getPropertyPath()
+  {
+    // TODO: Implement getPropertyPath() method.
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function setContext(
+    $name = null,
+    TraversableTypedDataInterface $parent = null
+  ) {
+    // TODO: Implement setContext() method.
+  }
+}
---
+++ modules/custom/dkan_search_api/src/Plugin/search_api/datasource/DkanData.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace Drupal\dkan_search_api\Plugin\search_api\datasource;
+
+use Drupal\Core\TypedData\ComplexDataInterface;
+use Drupal\dkan_search_api\ComplexData\Dataset;
+use Drupal\search_api\Datasource\DatasourcePluginBase;
+
+/**
+ * Represents a datasource which exposes DKAN data.
+ *
+ * @SearchApiDatasource(
+ *   id = "dkan_dataset",
+ *   label = "DKAN Dataset",
+ * )
+ */
+class DkanDataset extends DatasourcePluginBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getPropertyDefinitions() {
+    return Dataset::definition();
+  }
+
+  public function getItemIds($page = null)
+  {
+    global $firstTime;
+
+    if (!isset($page) || $page == 0) {
+      /* @var  $dataStorage  \Drupal\dkan_data\Storage\Data */
+      $dataStorage = \Drupal::service("dkan_data.storage");
+      $dataStorage->setSchema('dataset');
+      $objects = $dataStorage->retrieveAll();
+      $ids = array_map(function ($json) {
+        $object = json_decode($json);
+        return $object->identifier;
+      }, $objects);
+      $firstTime = FALSE;
+      return $ids;
+    }
+    return NULL;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function loadMultiple(array $ids) {
+    /* @var  $dataStorage  \Drupal\dkan_data\Storage\Data */
+    $dataStorage = \Drupal::service("dkan_data.storage");
+    $dataStorage->setSchema('dataset');
+
+    $items = [];
+    foreach ($ids as $id) {
+      $items[$id] = new Dataset($dataStorage->retrieve($id));
+    }
+
+    return $items;
+  }
+
+
+  /**
+   * @inheritDoc
+   */
+  public function getItemId(ComplexDataInterface $item)
+  {
+    return $item->get('identifier');
+  }
+}
