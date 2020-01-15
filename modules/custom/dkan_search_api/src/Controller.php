44a93de19fd957ceab04cfe2388b0ef52fadaf5b
fmizzell <fmizzell.dev@gmail.com>
2020-01-14 23:45:54 -0600

    First draft of search api integration.

---
+++ modules/custom/dkan_search_api/src/Controller.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Drupal\dkan_search_api;
+
+use Drupal\dkan_common\JsonResponseTrait;
+use Drupal\dkan_metastore\Service;
+use Drupal\search_api\Query\ResultSet;
+
+class Controller
+{
+  use JsonResponseTrait;
+
+  public function search() {
+    $defaults = [
+      "pageSize" => 10,
+      "page" => 1
+    ];
+
+    $params = \Drupal::request()->query->all();
+
+    foreach ($defaults as $param => $default) {
+      $params[$param] = isset($params[$param]) ? $params[$param] : $default;
+    }
+
+    /* @var $qh \Drupal\search_api\Utility\QueryHelper */
+    $qh = \Drupal::service("search_api.query_helper");
+
+    $storage = \Drupal::service("entity.manager")->getStorage('search_api_index');
+
+    /** @var  $metastore Service */
+    $metastore = \Drupal::service("dkan_metastore.service");
+
+    /** @var \Drupal\search_api\IndexInterface $index */
+    $index = $storage->load('index_1');
+
+    $query = $qh->createQuery($index);
+
+    if ($params['fulltext']) {
+      $fulltextFields = $index->getFulltextFields();
+      $cg = $query->createConditionGroup('OR');
+      foreach ($fulltextFields as $field) {
+        $cg->addCondition($field, $params['fulltext']);
+      }
+      $query->addConditionGroup($cg);
+    }
+
+    $query->sort('search_api_relevance', $query::SORT_DESC);
+
+    $end = ($params['page'] * $params['pageSize']);
+    $start = $end - $params['pageSize'];
+    $query->range($start, $params['pageSize']);
+
+    /** @var  $result ResultSet*/
+    $result = $query->execute();
+
+    $data = [];
+    $scores = [];
+    foreach ($result->getResultItems() as $item) {
+      $id = $item->getId();
+      $id = str_replace("dkan_data/", "", $id);
+      $scores[$id] = $item->getBoost();
+      $data[] = json_decode($metastore->get("dataset", $id));
+    }
+
+    return $this->getResponse($data);
+  }
+}
