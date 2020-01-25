bb5f9cd937f212966a559bc1f7458e9f9440bde3
fmizzell <fmizzell.dev@gmail.com>
2020-01-25 12:15:33 -0600

    React json schema form in Drupal.

---
+++ modules/custom/dkan_json_form/js/app/build/precache-manifest.5226a40a71d63a44c0a747156a16adae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "053d7549c985f2936213cc551a176fc6",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/index.html"
+  },
+  {
+    "revision": "4e960065c39b98ca9f0a",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "294f3c28ca4108460370",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/2.692317f6.chunk.js"
+  },
+  {
+    "revision": "4389e93565403b0e2df7f176d01e62bd",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/2.692317f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4e960065c39b98ca9f0a",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/main.2314b2f0.chunk.js"
+  },
+  {
+    "revision": "8ea8bfb212412458cf5a",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/runtime-main.ca6f5c2c.js"
+  }
+]);
--- modules/custom/dkan_json_form/js/app/src/App.js
+++ modules/custom/dkan_json_form/js/app/src/App.js
@@ -1,25 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import Form from "react-jsonschema-form";
+const axios = require('axios');
 
 function App() {
+
+  const [schema, setSchema] = useState({})
+  const [formData, setFormData] = useState({})
+
+
+  useEffect(() => {
+    async function fetchSchema() {
+      const response = await axios.get('/api/1/metastore/schemas/dataset');
+      setSchema(response.data);
+
+      const id = getId()
+      if (id) {
+        const response2 = await axios.get('/api/1/metastore/schemas/dataset/items/' + id);
+        setFormData(response2.data);
+      }
+    }  
+  
+    fetchSchema();
+  }, []);
+
+  function submitDataset(event) {
+    const id = getId();
+    if (id) {
+      axios.put('/api/1/metastore/schemas/dataset/items/' + id, event.formData);
+    }
+    else {
+      axios.post('/api/1/metastore/schemas/dataset/items', event.formData);
+    }
+  }
+
+  function getId() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const ids = urlParams.getAll('id');
+    if (ids.length > 0) {
+      return ids[0];
+    }
+    return null;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Form schema={schema} formData={formData}
+        onSubmit={ (e) => {submitDataset(e)} }
+        onError={(e) => { console.log(e)}} />
   );
 }
 
