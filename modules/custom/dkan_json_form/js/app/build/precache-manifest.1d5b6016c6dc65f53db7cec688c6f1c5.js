01a7f441222b87e719a3d791594df26fd3ede556
fmizzell <fmizzell.dev@gmail.com>
2020-02-02 15:51:36 -0600

    New app.

---
+++ modules/custom/dkan_json_form/js/app/build/precache-manifest.1d5b6016c6dc65f53db7cec688c6f1c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c184c8ed9b66d0714064b13c2818b9dd",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/index.html"
+  },
+  {
+    "revision": "9148ff1d89ea91f6b87e",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/css/2.52734cf0.chunk.css"
+  },
+  {
+    "revision": "9148ff1d89ea91f6b87e",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/2.6c1412fb.chunk.js"
+  },
+  {
+    "revision": "169e542ed27e66e9aa7d48e5c3d9b7f3",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/2.6c1412fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5c367f776a551191fcd",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/main.6a03990e.chunk.js"
+  },
+  {
+    "revision": "8ea8bfb212412458cf5a",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/runtime-main.ca6f5c2c.js"
+  }
+]);
