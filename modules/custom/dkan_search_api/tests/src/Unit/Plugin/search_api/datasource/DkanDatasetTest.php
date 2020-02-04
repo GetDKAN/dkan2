618a6c1564e593017762b1c9d8018e0537373b5f
fmizzell <fmizzell.dev@gmail.com>
2020-02-04 11:02:40 -0600

    Increase code coverage.

--- modules/custom/dkan_search_api/tests/src/Unit/Plugin/search_api/datasource/DkanDatasetTest.php
+++ modules/custom/dkan_search_api/tests/src/Unit/Plugin/search_api/datasource/DkanDatasetTest.php
@@ -4,6 +4,7 @@ use Drupal\Core\DependencyInjection\Container;
 use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Entity\EntityTypeManager;
 use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\dkan_data\Storage\Data;
 use Drupal\dkan_search_api\Plugin\search_api\datasource\DkanDataset;
 use Drupal\node\NodeInterface;
 use MockChain\Chain;
@@ -11,6 +12,7 @@ use MockChain\Options;
 use MockChain\Sequence;
 use PHPUnit\Framework\TestCase;
 use Drupal\Core\Entity\EntityTypeRepository;
+use Drupal\dkan_search_api\ComplexData\Dataset;
 
 /**
  *
@@ -23,7 +25,8 @@ class DkanDatasetTest extends TestCase {
   public function test() {
     $containerOptions = (new Options())
       ->add('entity_type.manager', EntityTypeManager::class)
-      ->add('entity_type.repository', EntityTypeRepository::class);
+      ->add('entity_type.repository', EntityTypeRepository::class)
+      ->add('dkan_data.storage', Data::class);
 
     $nids = [1, 2];
     $executeSequence = (new Sequence())->add(2)->add($nids);
@@ -38,6 +41,7 @@ class DkanDatasetTest extends TestCase {
       ->add(QueryInterface::class, 'execute', $executeSequence)
       ->add(QueryInterface::class, 'range', QueryInterface::class)
       ->add(EntityTypeRepository::class, 'getEntityTypeFromClass', NULL)
+      ->add(Data::class, 'retrieve', '{}')
       ->getMock();
 
     \Drupal::setContainer($container);
@@ -45,6 +49,9 @@ class DkanDatasetTest extends TestCase {
     $plugin = new DkanDataset([], 'id', []);
     $ids = $plugin->getItemIds(0);
     $this->assertEquals(json_encode(['xyz', 'xyz']), json_encode($ids));
+
+    $items = $plugin->loadMultiple($ids);
+    $this->assertEquals(Dataset::class, get_class($items['xyz']));
   }
 
 }
