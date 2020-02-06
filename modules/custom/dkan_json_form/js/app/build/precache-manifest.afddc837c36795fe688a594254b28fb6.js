e08f414ea66d2c9c9b1481b5422be3954e3d53f5
Janette Day <janette.day@civicactions.com>
2020-02-05 23:30:27 -0600

    run build

---
+++ modules/custom/dkan_json_form/js/app/build/precache-manifest.afddc837c36795fe688a594254b28fb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe8d206d3ec031137b6c62f93482e5c",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/index.html"
+  },
+  {
+    "revision": "a23c0cb7a464b069ddcd",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/css/2.52734cf0.chunk.css"
+  },
+  {
+    "revision": "8922ba685e52a182652d",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/css/main.7095cb78.chunk.css"
+  },
+  {
+    "revision": "a23c0cb7a464b069ddcd",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/2.630aa503.chunk.js"
+  },
+  {
+    "revision": "169e542ed27e66e9aa7d48e5c3d9b7f3",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/2.630aa503.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8922ba685e52a182652d",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/main.8b19c55b.chunk.js"
+  },
+  {
+    "revision": "8ea8bfb212412458cf5a",
+    "url": "/profiles/contrib/dkan2/modules/custom/dkan_json_form/js/app/build/static/js/runtime-main.ca6f5c2c.js"
+  }
+]);
