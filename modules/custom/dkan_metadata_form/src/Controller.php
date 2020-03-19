52e496e53122bad744f99fe785e2bf65d61c50ce
Janette Day <janette.day@civicactions.com>
2020-03-19 15:24:52 -0500

    Fix dependency injection problem

--- modules/custom/dkan_metadata_form/src/Controller.php
+++ modules/custom/dkan_metadata_form/src/Controller.php
@@ -3,20 +3,43 @@
 namespace Drupal\dkan_metadata_form;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Drupal\Component\Uuid\Php;
 
 /**
  * Controller.
  */
 class Controller extends ControllerBase {
+  
+  /**
+   * @var Php $uuidService
+   */
+  protected $uuidService;
+
+  /**
+   * Class constructor.
+   */
+  public function __construct(Php $uuid) {
+    $this->uuidService = $uuid;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    // Instantiates this form class.
+    return new static(
+      // Load the service required to construct this class.
+      $container->get('uuid')
+    );
+  }
 
   /**
    * Page.
    */
   public function page() {
-
     // Generate a uuid for new datasets, pass it to the form.
-    $uuid_service = \Drupal::service('uuid');
-    $uuid = $uuid_service->generate();
+    $uuid = $this->uuidService->generate();
 
     return [
       '#markup' => '<div id="app"></div>',
@@ -25,9 +48,10 @@ class Controller extends ControllerBase {
           'dkan_metadata_form/dkan_metadata_form',
         ],
         'drupalSettings' => [
-          'tempUUID' => $uuid
-        ]
+          'tempUUID' => $uuid,
+        ],
       ],
     ];
   }
+
 }
